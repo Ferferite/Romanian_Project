85f87b8be8bda5d43b3db16fc586c84762228533
Ferferite <Angheluta.andrei@outlook.com>
2021-10-15 18:43:53 +0300

    added choice

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from requests_html import HTML, HTMLSession
 import random
 
-#listacuvinte=["grai","frumos","descoperim","sfiala","poveste","cancer","suparat","destept","banuit","voce","marunte","mireasma","spaima","stralucire","destept","suras","omat","lent","cantec","copil","sclipitor"]
-listacuvinte=["Mireasma"]
+def incercare():
+    alegere = input("Alege un sinonim pentru cuvantul {cuvant}, din urmatoarele sinonime: {cuvantFals}, {cuvantReal}, {cuvantFals2}: ".format(cuvant=cuvantRandom, cuvantFals=random.choice(listacuvinte), cuvantReal='muie', cuvantFals2=random.choice(listacuvinte))).lower()
+    if alegere in sinonime:
+        print('Bravo!')
+    else:
+        print('Mai Incerca!')
+        incercare()
+
+listacuvinte=["grai","frumos","descoperim","sfiala","poveste","cancer","suparat","destept","banuit","voce","marunte","mireasma","spaima","stralucire","destept","suras","omat","lent","cantec","copil","sclipitor"]
+#listacuvinte=["Mireasma"]
 cuvantRandom = random.choice(listacuvinte)
 sinonime=["miros", "aroma"]
 session = HTMLSession()
 url = 'https://www.sin0nime.com/dex/?cheie='+cuvantRandom
 r = session.get(url)
-alegere = input('Sinonim pentru cuvantul "'+cuvantRandom+'": ').lower()
-if alegere in sinonime:
-    print('Bravo!')
-else:
-    print('Mai Incerca!')
+incercare()
 print(r.html)
