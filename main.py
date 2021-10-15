099acf0cf8d2b67908ac190e97480661be427e9f
Ferferite <Angheluta.andrei@outlook.com>
2021-10-16 02:34:34 +0300

    getting synonims from dexonline.ro, need to fix formatting/unicode bug

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from requests_html import HTML, HTMLSession
 import random
+from requests_html import HTML, HTMLSession
 
 def incercare():
-    alegere = input("Alege un sinonim pentru cuvantul {cuvant}, din urmatoarele sinonime: {cuvantFals}, {cuvantReal}, {cuvantFals2}: ".format(cuvant=cuvantRandom, cuvantFals=random.choice(listacuvinte), cuvantReal='muie', cuvantFals2=random.choice(listacuvinte))).lower()
-    if alegere in sinonime:
+    alegere = input("Alege un sinonim pentru cuvantul {cuvant}, din urmatoarele sinonime: {cuvantFals}, {cuvantReal}, {cuvantFals2}: ".format(cuvant=cuvantRandom, cuvantFals=random.choice(listacuvinte), cuvantReal=cuvantReal, cuvantFals2=random.choice(listacuvinte))).lower()
+    if alegere in sinonim:
         print('Bravo!')
     else:
         print('Mai Incerca!')
         incercare()
 
-listacuvinte=["grai","frumos","descoperim","sfiala","poveste","cancer","suparat","destept","banuit","voce","marunte","mireasma","spaima","stralucire","destept","suras","omat","lent","cantec","copil","sclipitor"]
-#listacuvinte=["Mireasma"]
+#listacuvinte=["grai","frumos","descoperim","sfiala","poveste","destept","banuit","voce","marunte","mireasma","spaima","stralucire","destept","suras","omat","lent"]
+listacuvinte=["grai"]
 cuvantRandom = random.choice(listacuvinte)
-sinonime=["miros", "aroma"]
+url = 'https://dexonline.ro/definitie-sinonime/'+cuvantRandom+'/json'
 session = HTMLSession()
-url = 'https://www.sin0nime.com/dex/?cheie='+cuvantRandom
 r = session.get(url)
-incercare()
 print(r.html)
+sinonim = str(r.html.search('${}.$')[0]).replace(u"\u0103", "a")
+cuvantReal=sinonim.split(',')[-1]
+print(cuvantReal)
+incercare()
+
+
+
