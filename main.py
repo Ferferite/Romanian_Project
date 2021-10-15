043b025c40495405631701ad2358bd8669737b3d
Ferferite <Angheluta.andrei@outlook.com>
2021-10-15 18:26:18 +0300

    added the rest of the code, still need to figure out scraping

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from requests_html import HTML, HTMLSession
+import random
 
+#listacuvinte=["grai","frumos","descoperim","sfiala","poveste","cancer","suparat","destept","banuit","voce","marunte","mireasma","spaima","stralucire","destept","suras","omat","lent","cantec","copil","sclipitor"]
+listacuvinte=["Mireasma"]
+cuvantRandom = random.choice(listacuvinte)
+sinonime=["miros", "aroma"]
 session = HTMLSession()
-r = session.get('https://www.sin0nime.com/dex/?cheie=piatra')
-
-#cuvant = r.html.find('a', first=True)
-cuvant = r.html.find('Sinonime', first=True)
-print(cuvant.html)
+url = 'https://www.sin0nime.com/dex/?cheie='+cuvantRandom
+r = session.get(url)
+alegere = input('Sinonim pentru cuvantul "'+cuvantRandom+'": ').lower()
+if alegere in sinonime:
+    print('Bravo!')
+else:
+    print('Mai Incerca!')
 print(r.html)
