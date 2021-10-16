fb8b3d158fd2f69fdeb50724847b9e78f8f2818f
bfg <bflorin519@gmail.com>
2021-10-16 12:09:51 +0300

    small ui improvement

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 import unidecode
+import os
 from requests_html import HTML, HTMLSession
 
 def incercare():
-    alegere = input("Alege un sinonim pentru cuvantul {cuvant}, din urmatoarele sinonime: {cuvantFals}, {cuvantReal}, {cuvantFals2}: ".format(cuvant=cuvantRandom, cuvantFals=random.choice(listacuvinte), cuvantReal=cuvantReal, cuvantFals2=random.choice(listacuvinte))).lower()
+    os.system("color b0")
+    os.system("cls")
+    alegere = input("   Alege un sinonim pentru cuvantul {cuvant}, din urmatoarele sinonime: {cuvantFals},{cuvantReal}, {cuvantFals2}: ".format(cuvant=cuvantRandom, cuvantFals=random.choice(listacuvinte), cuvantReal=cuvantReal, cuvantFals2=random.choice(listacuvinte))).lower()
     if alegere in sinonim:
         print('Bravo!')
     else:
@@ -16,11 +19,11 @@ cuvantRandom = random.choice(listacuvinte)
 url = 'https://dexonline.ro/definitie-sinonime/'+cuvantRandom+'/json'
 session = HTMLSession()
 r = session.get(url)
-print(r.html)
+#print(r.html)
 #sinonim = str(r.html.search('${}.$')[0]).replace(u"\u0103", "a")
 sinonim = str(r.html.search('${}.$')[0].encode('utf-8').decode('unicode-escape'))
 cuvantReal=sinonim.split(',')[-1]
-print(cuvantReal)
+#print(cuvantReal)
 incercare()
 
 
